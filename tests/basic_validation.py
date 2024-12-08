45548f88dba431a0d9d9bf746f98c8695a96d379
dpatel2311 <163790734+dpatel2311@users.noreply.github.com>
2024-12-08 13:23:47 -0500

    Update basic_validation.py

--- tests/basic_validation.py
+++ tests/basic_validation.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 import pytest
 from unittest.mock import Mock
-from my_function_app import main  # Adjust the import based on your file structure
+from function_app import main  # Adjust the import based on your file structure
 
 def test_main():
     # Create a mock HTTP request
